e72b8c7c90dde03c9d1661f8040cbebbbc3a1b8e
Chekwube <chekwube@rubikpay.net>
2021-03-04 01:02:01 +0100

    Added product and review screens and added router

---
+++ frontend/src/data.js
@@ -0,0 +1,84 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product"
+    },
+
+    {
+      _id: "2",
+      name: "Adidas fit Shirt",
+      category: "Shirts",
+      image: "/images/p2.jpg",
+      price: 100,
+      countInStock: 20,
+
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 100,
+      description: "high quality product"
+    },
+    {
+      _id: "3",
+      name: "Lacoste free Shirt",
+      category: "Shirts",
+      image: "/images/p3.jpg",
+      price: 150,
+      countInStock: 0,
+
+      brand: "Lacoste",
+      rating: 4.8,
+      numReviews: 10,
+      description: "high quality product"
+    },
+    {
+      _id: "4",
+      name: "Nike Slim pant",
+      category: "pants",
+      image: "/images/p4.jpg",
+      price: 78,
+      countInStock: 15,
+
+      brand: "Nike",
+      rating: 4.1,
+      numReviews: 13,
+      description: "high quality product"
+    },
+    {
+      _id: "5",
+      name: "Puma Slim Shirt",
+      category: "pants",
+      image: "/images/p5.jpg",
+      price: 190,
+      countInStock: 5,
+
+      brand: "Puma",
+      rating: 4.0,
+      numReviews: 100,
+      description: "high quality product"
+    },
+    {
+      _id: "6",
+      name: "Adidas Slim Shirt",
+      category: "Shirts",
+      image: "/images/p6.jpg",
+      price: 170,
+      countInStock: 12,
+
+      brand: "Nike",
+      rating: 4.9,
+      numReviews: 16,
+      description: "high quality product"
+    }
+  ]
+}
+
+export default data;
